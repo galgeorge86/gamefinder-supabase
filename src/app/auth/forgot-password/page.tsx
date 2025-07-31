4fb6ce0cbff9579ce1820fdbe291a7e83cf0ec59
galgeorge86 <50632904+galgeorge86@users.noreply.github.com>
2025-08-01 01:39:56 +0300

    Removed form fade-in animation for performance purposes

--- src/app/auth/forgot-password/page.tsx
+++ src/app/auth/forgot-password/page.tsx
@@ -1,18 +1,11 @@
 'use client'
 import ForgotPasswordForm from "./forgot-password-form";
-import {motion} from 'framer-motion'
 
 export default function SignInPage () {
 
     return (
         <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-gradient-to-br from-primary-100 to-primary dark:to-primary">
-            <motion.div 
-                initial={{opacity: 0}}
-                animate={{opacity: 1}}
-                exit={{opacity: 0}}
-                transition={{duration: 0.2}}>
             <ForgotPasswordForm/>
-            </motion.div>
         </main>
     )
 }
--- src/app/auth/sign-in/page.tsx
+++ src/app/auth/sign-in/page.tsx
@@ -1,18 +1,10 @@
 'use client'
 import SignInForm from "./sign-in-form";
-import {motion} from 'framer-motion'
 
 export default function SignInPage () {
-
     return (
         <main className="flex flex-col items-center justify-center min-h-screen p-4 bg-gradient-to-br from-primary-100 to-primary dark:to-primary">
-            <motion.div 
-                initial={{opacity: 0}}
-                animate={{opacity: 1}}
-                exit={{opacity: 0}}
-                transition={{duration: 0.2}}>
             <SignInForm/>
-            </motion.div>
         </main>
     )
 }
