cdce23c3ceeb8ac88f91ae3bfe19c0523bc91d3b
galgeorge86 <50632904+galgeorge86@users.noreply.github.com>
2025-08-07 05:18:59 +0300

    Fixed previously set margins

--- src/app/user/explore/page.tsx
+++ src/app/user/explore/page.tsx
@@ -7,7 +7,7 @@ export default function ExplorePage() {
   return (
     <GeneralLayout>
       {/* Adjusted absolute margins so Mapbox logo and credits are visible on mobile */}
-      <div className="absolute top-0 md:bottom-0 bottom-[72px] left-0 right-0 ">
+      <div className="absolute top-0 lg:bottom-0 bottom-[72px] left-0 right-0 ">
         <MapSection/>
       </div>
     </GeneralLayout>
